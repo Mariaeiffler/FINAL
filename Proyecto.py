c3cab8857edd4254f20e1c7c6f416b6da0700e8b
mariaeiffler <meiffler@itba.edu.ar>
2023-12-02 18:30:16 -0300

    bajada de datos

--- Proyecto.py
+++ Proyecto.py
@@ -1,6 +1,6 @@
 
 class Proyecto():
-    def __init__(self,proyecto_id,proyecto_fuente,titulo,fecha_inicio,fecha_finalizacion,resumen,moneda_id,monto_total_solicitado,monto_total_adjudicado,monto_financiado_solicitado,monto_financiado_adjudicado,tipo_proyecto_id,codigo_identificacion,palabras_clave,estado_id,fondo_anpcyt,cantidad_miembros_F,cantidad_miembros_M,sexo_director,anio):
+    def __init__(self,proyecto_id,proyecto_fuente,titulo,fecha_inicio,fecha_finalizacion,resumen,moneda_id,monto_total_solicitado,monto_total_adjudicado,monto_financiado_solicitado,monto_financiado_adjudicado,tipo_proyecto_id,codigo_identificacion,palabras_clave,estado_id,fondo_anpcyt,cantidad_miembros_F,cantidad_miembros_M,sexo_director,anio,disciplina):
         self.proyecto_id = proyecto_id
         self.proyecto_fuente = proyecto_fuente
         self.titulo = titulo
@@ -21,3 +21,4 @@ class Proyecto():
         self.cantidad_miembros_M = cantidad_miembros_M
         self.sexo_director = sexo_director
         self.anio = anio
+        self.disciplina = disciplina
